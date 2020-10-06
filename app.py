c7ca763c4c7918f3581a9655d46e111a792a2b34
Mikhail Laptev <27733116+bytemux@users.noreply.github.com>
2020-10-06 19:09:36 +0300

    init flask & config debug location

---
+++ app.py
@@ -0,0 +1,26 @@
+import os
+import logging
+import json
+from flask import Flask
+app = Flask(__name__)
+
+# Select config
+try:
+    env = os.environ['APP_ENV']
+except KeyError as e:
+    logging.error('Unknown environment key, defaulting to Development')
+    env = 'DevelopmentConfig'
+    app.config.from_object('config.%s' % env)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+@app.route('/flask_config')
+def flask_config():
+    return json.dumps(app.config, indent=4, sort_keys=True, default=str)
+
+
+if __name__ == '__main__':
+    app.run()
+
