683c1fe0bce59618d708dead6453051dddf3ea96
coderoque <coderoque@gmail.com>
2020-10-09 20:11:11 +0300

    project struture changed

--- reviews/views.py
+++ reviews/views.py
@@ -3,6 +3,6 @@ from flask import Blueprint
 reviews = Blueprint('reviews', __name__)
 
 
-@reviews.route('/handshake')
-def main():
+@reviews.route('/handshake/')
+def handshake():
     return {'hand': 'shake'}
--- users/views.py
+++ users/views.py
@@ -3,6 +3,6 @@ from flask import Blueprint
 users = Blueprint('users', __name__)
 
 
-@users.route('/handshake')
-def main():
+@users.route('/handshake/')
+def handshake():
     return {'hand': 'shake'}
