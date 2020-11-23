620661f04a2df00d315d7a4cf54df28e9a3bb350
Pavel Kuchin <pvlkuchin@gmail.com>
2020-11-23 20:41:51 +0300

    chore(fe): adjust integration with backend

--- frontend/src/features/spots/api.ts
+++ frontend/src/features/spots/api.ts
@@ -2,33 +2,6 @@ import { request } from 'features/core'
 
 import type { TSpot } from './types'
 
-type TRawSpot = {
-  latitude?: string
-  longitude?: string
-  id: number
-  name?: string
-}
+export const getSpot = (id: number) => request.get<TSpot>(`/api/spots/${id}`).catch(() => null)
 
-const processSpot = ({ id, latitude, longitude, name }: TRawSpot) => {
-  const lt = String(latitude)
-  const lg = String(longitude)
-  return { id, name, latitude: parseFloat(lt), longitude: parseFloat(lg) }
-}
-
-export const getSpot = async (id: number) => {
-  try {
-    const rawSpot = await request.get<TRawSpot>(`/api/spots/${id}`)
-    return processSpot(rawSpot)
-  } catch (e) {
-    return null
-  }
-}
-
-export const getSpots = async () => {
-  try {
-    const rawSpots = await request.get<TRawSpot[]>('/api/spots')
-    return rawSpots.map(processSpot)
-  } catch (e) {
-    return []
-  }
-}
+export const getSpots = async () => request.get<TSpot[]>('/api/spots').catch(() => [] as TSpot[])
